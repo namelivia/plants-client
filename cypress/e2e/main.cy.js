ef414600f2db2c4c6d67f64f05a23483c86c5a1d
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-06-02 17:41:27 +0200

    Unskip test

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -30,40 +30,47 @@ describe('e2e tests', () => {
     cy.contains('Water all plants').click()
   })
 
-    it('When marking one plant as watered the card is dismissed', () => {
-        cy.intercept('GET', 'https://plants.localhost.pomerium.io/api/users/me', {
-            fixture: 'users/me',
-        }).as('getMe')
-        cy.intercept(
-            'GET',
-            'https://plants.localhost.pomerium.io/api/plants/to_be_watered',
-            { fixture: 'plants/plants_to_be_watered' },
-        ).as('getPlantsToBeWatered')
-        cy.intercept(
-            'POST',
-            'https://plants.localhost.pomerium.io/api/plants/water_all',
-            { statusCode: 204 },
-        ).as('waterAllPlants')
-        cy.visit('/')
-        cy.get('.card').should('have.length', 2)
-        cy.intercept('POST', 'https://plants.localhost.pomerium.io/api/plants/1/water', {
-            statusCode: 200,
-        }).as('waterPlant')
-        cy.findAllByText('Water it').first().click()
-        cy.get('.card').should('have.length', 1)
-        
-    })
+  it('When marking one plant as watered the card is dismissed', () => {
+    cy.intercept('GET', 'https://plants.localhost.pomerium.io/api/users/me', {
+      fixture: 'users/me',
+    }).as('getMe')
+    cy.intercept(
+      'GET',
+      'https://plants.localhost.pomerium.io/api/plants/to_be_watered',
+      { fixture: 'plants/plants_to_be_watered' },
+    ).as('getPlantsToBeWatered')
+    cy.intercept(
+      'POST',
+      'https://plants.localhost.pomerium.io/api/plants/water_all',
+      { statusCode: 204 },
+    ).as('waterAllPlants')
+    cy.visit('/')
+    cy.get('.card').should('have.length', 2)
+    cy.intercept(
+      'POST',
+      'https://plants.localhost.pomerium.io/api/plants/1/water',
+      {
+        statusCode: 200,
+      },
+    ).as('waterPlant')
+    cy.findAllByText('Water it').first().click()
+    cy.get('.card').should('have.length', 1)
+  })
 
-  it.only('Plant details page', () => {
+  it('Plant details page', () => {
     cy.intercept('GET', 'https://plants.localhost.pomerium.io/api/users/me', {
       fixture: 'users/me',
     }).as('getMe')
     cy.intercept('GET', 'https://plants.localhost.pomerium.io/api/plants/1', {
       fixture: 'plants/plant_details',
     }).as('getPlant')
-    cy.intercept('GET', 'https://plants.localhost.pomerium.io/api/plants/1/journal', {
-      fixture: 'plants/plant_journal',
-    }).as('getPlantJournal')
+    cy.intercept(
+      'GET',
+      'https://plants.localhost.pomerium.io/api/plants/1/journal',
+      {
+        fixture: 'plants/plant_journal',
+      },
+    ).as('getPlantJournal')
     cy.intercept(
       'POST',
       'https://plants.localhost.pomerium.io/api/plants/1/journal',
